30a328fb0ecad459a85e45b62bbc0a11258d4172
Johannes Rückert <rawbauke@gmail.com>
2015-10-26 19:58:56 +0100

    liquid can get the current day/year, no need for js

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,22 +47,5 @@ $(function() {
     $('.js-latest-commit_sha').append(window.app.commit.sha);
     $('.js-latest-commit_name').append(window.app.commit.name);
   }
-  
-  // get the current day (used in footer message)
-  var date = new Date();
-  var weekday = new Array(7);
-   weekday[0] = "sunday";
-   weekday[1] = "monday";
-   weekday[2] = "tuesday";
-   weekday[3] = "wednesday";
-   weekday[4] = "thursday";
-   weekday[5] = "friday";
-   weekday[6] = "saturday";
-
-  var now = weekday[date.getDay()];
-
-  $('.js-current-day').append(now);
-
-
 }); // ready-function
 
